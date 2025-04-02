60df863c056b2a5464ff43cd89fe51c772cb7269
NguyenPhat <nguyenphatssj0612@gmail.com>
2025-04-02 14:29:25 +0700

    a

--- User.js
+++
@@ -1,8 +0,0 @@
-class User {
-  constructor(userId, name, username, password) {
-    this.userId = userId;
-    this.name = name;
-    this.username = username;
-    this.password = password;
-  }
-}
